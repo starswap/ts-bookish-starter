ffd41f33a4a7ccaba37e27c62c5c1a3d9bff9e9f
Jamie Turner <jamie.turner@softwire.com>
2023-07-07 16:45:42 +0100

    Feat: Adds JWT support without checking password

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,26 @@ import bookRoutes from './controllers/bookController';
 import { getBooks } from './db';
 import {sign, verify} from 'jsonwebtoken';
 import fs from 'fs';
+import {Strategy, ExtractJwt} from 'passport-jwt'
+import passport from 'passport'
+
+function init_passport() {
+    let pubKey = fs.readFileSync("jwt-rs256.pub");
+    const ops = {
+        jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey : pubKey,
+        algorithms : ["RS256"] 
+    }
+    
+    const strategy = new Strategy(ops, (payload, done)=>{
+        done(null, payload.username)     
+    })
+    
+    passport.use(strategy);
+}
+init_passport();
 
 const port = process.env['PORT'] || 3000;
-
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
@@ -16,28 +33,19 @@ app.listen(port, () => {
 
 app.use('/healthcheck', healthcheckRoutes);
 app.use('/books', bookRoutes);
-app.get('/', async (req, res) => {
+app.get('/', passport.authenticate("jwt", {session : false}), async (req, res) => {
     res.send(await getBooks());
 });
+
 app.post('/login', async (req, res) => {
- //   req.body.username;
-
-
     const username = req.body["username"];
     const password = req.body["password"];
-    if (!username){
-        res.status(400).send("No username")
-        return;
+    if (!username || !password){
+        res.status(400).send({success: false, message: "provide username and password fields"});
+    } else {
+        let privKey = fs.readFileSync("jwt-rs256.key");
+        let token = sign({username : username}, privKey, {algorithm : 'RS256'});
+        res.send({token : token});
     }
-    if (!password){
-        res.status(400).send("No password")
-        return;
-    }
-    let privKey = fs.readFileSync("jwt-rs256.key");
-    let token = sign({username : username}, privKey, {algorithm : 'RS256'});
-
-    res.send({token : token});
-
-
 });
 
