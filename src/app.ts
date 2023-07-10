6ff30a5511563b11951e4af74eefe60145dbad6a
Hamish Starling <hamish.starling@softwire.com>
2023-07-10 10:34:15 +0100

    Refactor: Moves get all books endpoint into bookController

--- src/app.ts
+++ src/app.ts
@@ -4,54 +4,37 @@ import userRoutes from './controllers/userController';
 import healthcheckRoutes from './controllers/healthcheckController';
 import bookRoutes from './controllers/bookController';
 import fs from 'fs';
-import {Strategy, ExtractJwt} from 'passport-jwt'
-import passport from 'passport'
-import { runQuery } from './db';
-import { Book } from './db_types';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import passport from 'passport';
 
-
+// Setup the passport authentication with JSON web tokens
 function init_passport() {
     let pubKey = fs.readFileSync("jwt-rs256.pub");
     const ops = {
-        jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
-        secretOrKey : pubKey,
-        algorithms : ["RS256"] 
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: pubKey,
+        algorithms: ["RS256"]
     }
-    
-    const strategy = new Strategy(ops, (payload, done)=>{
-        done(null, payload.username)     
+
+    const strategy = new Strategy(ops, (payload, done) => {
+        done(null, payload.username);
     })
-    
+
     passport.use(strategy);
 }
+
 init_passport();
 
+// Prepare the aplication
 const port = process.env['PORT'] || 3000;
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
-app.listen(port, () => {
-    return console.log(`Bookish Library API is listening at http://localhost:${port}`);
-});
-
-async function getBooks(): Promise<Book[]> {
-    const books = await runQuery<Book>("SELECT * FROM book;", (columns) => {
-        return {
-            title: columns[0].value,
-            isbn: columns[1].value
-        };
-    });
-    return books;
-} 
-
-
-
 app.use('/healthcheck', healthcheckRoutes);
 app.use('/books', bookRoutes);
 app.use('/user', userRoutes);
 
-app.get('/', passport.authenticate("jwt", {session : false}), async (req, res) => {
-    res.send({
-        "books": await getBooks()
-    });
+// Run the app!
+app.listen(port, () => {
+    return console.log(`Bookish Library API is listening at http://localhost:${port}`);
 });
-
--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,17 +1,30 @@
 import { Router, Request, Response } from 'express';
+import { runQuery } from '../db';
+import { Book } from '../db_types';
+import passport from 'passport';
 
 class BookController {
     router: Router;
 
     constructor() {
         this.router = Router();
+        this.router.get('/all', passport.authenticate("jwt", {session : false}), this.getAllBooks.bind(this));
         this.router.get('/:id', this.getBook.bind(this));
+    }
 
-        this.router.post('/', this.createBook.bind(this));
+    private async getBooksFromDB(): Promise<Book[]> {
+        const books = await runQuery<Book>("SELECT * FROM book;", (columns) => {
+            return {
+                title: columns[0].value,
+                isbn: columns[1].value
+            };
+        });
+        return books;
     }
 
     getBook(req: Request, res: Response) {
         // TODO: implement functionality
+        console.log("s")
         return res.status(500).json({
             error: 'server_error',
             error_description: 'Endpoint not implemented yet.',
@@ -20,11 +33,19 @@ class BookController {
 
     createBook(req: Request, res: Response) {
         // TODO: implement functionality
+        console.log("d")
         return res.status(500).json({
             error: 'server_error',
             error_description: 'Endpoint not implemented yet.',
         });
     }
+
+    async getAllBooks(req: Request, res: Response) {
+        return res.status(200).send({
+            "books": await this.getBooksFromDB()
+        });
+    };
+    
 }
 
 export default new BookController().router;
